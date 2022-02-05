a967f22641aa13c1bdc4276b241db1fd57d7aeb8
ShouryaSharma277 <shouryasharmadevelopment@gmail.com>
2022-02-05 11:53:36 +0530

    first and last commit

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"></link>
+
+      </Head>
+
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossOrigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossOrigin="anonymous"></script>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import CarForm from "../lib/CarForm"
+
+export default function Home() {
+  return (
+    <div className="mx-3">
+      <h1>Add a car</h1>
+      <CarForm />
+    </div>
+  )
+}
